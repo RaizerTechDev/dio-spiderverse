4fcf26c9d04ce49758b25f0bd8aa8e4a6813d832
Rafael Raizer <87991807+RafaRz76Dev@users.noreply.github.com>
2023-06-27 14:00:23 -0300

    Update index.tsx

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -39,7 +39,7 @@ export default function Carousel({heroes, activeId}: IProp){
     transitionAudio.play();
     const voiceAudio = voicesAudio[visibleItems[1].id];
     if(voiceAudio) {
-      voiceAudio.volume = 2.3;
+      voiceAudio.volume = 1.3;
       voiceAudio.muted = false;
       voiceAudio.play();
     }
